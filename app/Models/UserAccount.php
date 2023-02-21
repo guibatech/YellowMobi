bea194bc62ea016345a6fee26596f95ee8f1c104
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-21 13:52:14 -0300

    Password encryption at user account creation time.

--- app/Models/UserAccount.php
+++ app/Models/UserAccount.php
@@ -3,11 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model as Model;
+use Illuminate\Support\Facades\Hash as Hash;
+use Illuminate\Database\Eloquent\Casts\Attribute as Attribute;
 
 class UserAccount extends Model {
 
     protected $table = "user_accounts";
     protected $primaryKey = "id";
     public $timestamps = true;
+
+    public function password(): Attribute {
+
+        return new Attribute(
+            
+            set: function($value) {
+
+                return Hash::make($value);
+
+            }
+
+        );
+
+    }
 
 }
