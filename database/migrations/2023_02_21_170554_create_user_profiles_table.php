59a266650504b2e9807ab22c5d9e6579a5a19c92
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-21 17:09:57 -0300

    Table creation for user profiles.

---
+++ database/migrations/2023_02_21_170554_create_user_profiles_table.php
@@ -0,0 +1,26 @@
+<?php 
+
+use Illuminate\Support\Facades\Schema as Schema;
+use Illuminate\Database\Migrations\Migration as Migration;
+use Illuminate\Database\Schema\Blueprint as Blueprint;
+
+return new class extends Migration {
+
+    public function up() {
+
+        Schema::create('user_profiles', function(Blueprint $table) {
+
+            $table->id('id');
+            $table->date('date_of_birth');
+
+        });
+
+    }
+
+    public function down() {
+
+        Schema::dropIfExists('user_profiles');
+
+    }
+
+};
