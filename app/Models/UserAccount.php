137a607835ccfeea1d79f1ebb861aaf0376beae6
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-22 18:30:00 -0300

    Creation of secure token generation algorithm

--- app/Models/UserAccount.php
+++ app/Models/UserAccount.php
@@ -13,6 +13,44 @@ class UserAccount extends Authenticatable {
     protected $primaryKey = "id";
     public $timestamps = true;
 
+    public function __construct() {
+
+        $this->attributes['activation_token'] = $this->generateActivationToken(11111, 99999);
+
+    }
+
+    private function generateActivationToken(int $minimum, int $maximum): int {
+
+        $newToken = 0;
+        $secureToken = false;
+        $tries = 0;
+        
+        while (!$secureToken && $tries < 50) {
+
+            $tries += 1;    
+            $newToken = str_split(rand($minimum, $maximum), 1);
+
+            foreach($newToken as $position => $digit) {
+                
+                if ($position == 0) {
+                    continue;
+                }
+
+                if ($digit == $newToken[$position - 1]) {
+                    $secureToken = false;
+                } else {
+                    $secureToken = true;
+                    break;
+                }
+
+            }
+
+        }
+
+        return (int) implode("", $newToken);
+
+    }
+
     public function password(): Attribute {
 
         return new Attribute(
