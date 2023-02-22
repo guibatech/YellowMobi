3dbe771d0725340ec889667b67751150c6c47d6b
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-22 17:14:40 -0300

    Columns referring to user account activation.
    Fields referring to activation tokens and date/time of user account activation were created.

---
+++ database/migrations/2023_02_22_165140_add_activation_token_columns_user_accounts.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema as Schema;
+use Illuminate\Database\Migrations\Migration as Migration;
+use Illuminate\Database\Schema\Blueprint as Blueprint;
+
+return new class extends Migration {
+
+    public function up() {
+
+        if (Schema::hasTable('user_accounts')) {
+
+            Schema::table('user_accounts', function(Blueprint $table) {
+
+                $table->string('activation_token')->nullable();
+                $table->datetime('activation_at')->nullable();
+
+            });
+
+        }
+
+    }
+
+    public function down() {
+
+        if (Schema::hasColumn('user_accounts', 'activation_token')) {
+
+            Schema::table('user_accounts', function(Blueprint $table) {
+
+                $table->dropColumn('activation_token');
+
+            });
+
+        }
+
+        if (Schema::hasColumn('user_accounts', 'activation_at')) {
+
+            Schema::table('user_accounts', function(Blueprint $table) {
+
+                $table->dropColumn('activation_at');
+
+            });
+
+        }
+
+    }
+
+};
