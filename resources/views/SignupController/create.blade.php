186b1d2c8f3b6bb3d2bb80e73682109dc43ebb67
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-22 22:58:43 -0300

    Visual tweaks.

--- resources/views/SignupController/create.blade.php
+++ resources/views/SignupController/create.blade.php
@@ -1,7 +1,7 @@
 @extends('template')
 
 @section('title')
-Signup
+Sign up
 @endsection
 
 @section('body')
@@ -12,7 +12,7 @@ Signup
 
     <div class="mb-4 text-center">
         <hgroup>
-            <h1 class="display-3 fw-bold mb-3">Signup</h1>
+            <h1 class="display-3 fw-bold mb-3">Sign up</h1>
             <h2 class="display-6 lh-14">Get started by creating a <span class="fw-bold text-primary">{{config('app.name')}}</span> account.</h2>
         </hgroup>
     </div>
@@ -24,7 +24,7 @@ Signup
             @method('POST')
 
             <div class="form-floating mb-3">
-                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" id="name" value="{{old('name')}}" placeholder=" ">
+                <input type="text" class="form-control @error('name') has-error @enderror" name="name" id="name" value="{{old('name')}}" placeholder=" ">
                 <label for="name" class="form-label">Name</label>
                 @error('name')
                 <div class="form-text text-danger">{{$message}}</div>
@@ -33,7 +33,7 @@ Signup
 
             <div class="mb-3">
                 <div class="form-floating">
-                    <input type="date" class="form-control @error('dateOfBirth') is-invalid @enderror" name="dateOfBirth" id="dateOfBirth" value="{{old('dateOfBirth')}}" placeholder=" ">
+                    <input type="date" class="form-control @error('dateOfBirth') has-error @enderror" name="dateOfBirth" id="dateOfBirth" value="{{old('dateOfBirth')}}" placeholder=" ">
                     <label for="dateOfBirth" class="form-label">Date of birth</label>
                 </div>
                 @error('dateOfBirth')
@@ -44,7 +44,7 @@ Signup
 
             <div class="mb-3">
                 <div class="form-floating">
-                    <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="example@example.com" value="{{old('email')}}">
+                    <input type="email" class="form-control @error('email') has-error @enderror" name="email" id="email" placeholder="example@example.com" value="{{old('email')}}">
                     <label for="email" class="form-label">Email</label>
                 </div>
                 @error('email')
@@ -57,7 +57,7 @@ Signup
                 <div class="input-group">
                     <span class="input-group-text">@</span>
                     <div class="form-floating">
-                        <input type="text" class="form-control @error('username') is-invalid @enderror" name="username" id="username" value="{{old('username')}}" placeholder="@example">
+                        <input type="text" class="form-control @error('username') has-error @enderror" name="username" id="username" value="{{old('username')}}" placeholder="@example">
                         <label for="username" class="form-label">Username</label>
                     </div>
                 </div>
@@ -67,7 +67,7 @@ Signup
             </div>
 
             <div class="form-floating mb-3">
-                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" id="password" placeholder=" ">
+                <input type="password" class="form-control @error('password') has-error @enderror" name="password" id="password" placeholder=" ">
                 <label for="password" class="form-label">Password</label>
                 @error('password')
                 <div class="form-text text-danger">{{$message}}</div>
@@ -75,7 +75,7 @@ Signup
             </div>
 
             <div class="form-floating mb-3">
-                <input type="password" class="form-control @error('confirmPassword') is-invalid @enderror" name="confirmPassword" id="confirmPassword" placeholder=" ">
+                <input type="password" class="form-control @error('confirmPassword') has-error @enderror" name="confirmPassword" id="confirmPassword" placeholder=" ">
                 <label for="confirmPassword" class="form-label">Confirm password</label>
                 @error('confirmPassword')
                 <div class="form-text text-danger">{{$message}}</div>
