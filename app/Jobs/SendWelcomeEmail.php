3618a7077dc66e866dc8399cde74b8aa0e891668
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-23 13:44:36 -0300

    User activity log for sending activation token.
    A user activity log should be generated when the account activation token is sent to the user.

--- app/Jobs/SendWelcomeEmail.php
+++ app/Jobs/SendWelcomeEmail.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Bus\Dispatchable as Dispatchable;
 use Illuminate\Bus\Queueable as Queueable;
 use Illuminate\Support\Facades\Mail as Mail;
 use App\Mail\SignupMail as SignupMail;
+use App\Models\UserActivity as UserActivity;
 
 class SendWelcomeEmail implements ShouldQueue {
 
@@ -33,6 +34,8 @@ class SendWelcomeEmail implements ShouldQueue {
     public function handle(): void {
 
         Mail::to($this->email)->send(new SignupMail($this->name, $this->username, $this->activationToken));
+        
+        UserActivity::quickActivity("Sending account activation token.", "The token {$this->activationToken} was sent to the email {$this->email}.", $this->userId);
 
     }
 
