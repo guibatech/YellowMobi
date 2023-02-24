02f032bc95ae8e453411d4bd081306fdec25e182
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-24 12:16:28 -0300

    Creation of the HTML structure of the signin page.

--- resources/views/SigninController/create.blade.php
+++ resources/views/SigninController/create.blade.php
@@ -8,4 +8,19 @@ Sign in
 
     @include('Components.navbar')
 
+    <form action="{{route('accounts.signin.do')}}" method="POST">
+
+        @csrf
+        @method('POST')
+
+        <label for="credential">Email or username</label>
+        <input type="text" name="credential" id="credential">
+
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+
+        <input type="submit" value="sign in">
+
+    </form>
+
 @endsection
