d8432c0a105a7cdc3187ed205bbebf7dc0bcc032
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-26 13:06:34 -0300

    Cursor pointer in the menu.
    All menu items change cursor to pointer when the mouse hovers over them.

--- resources/views/Components/header.blade.php
+++ resources/views/Components/header.blade.php
@@ -12,12 +12,12 @@
                 <img src="https://avatars.githubusercontent.com/u/125931531?v=4" alt="mdo" class="photo-profile rounded-circle">
             </a>
                     
-            <ul class="dropdown-menu text-small shadow text-center" style="">
-                <li><a class="dropdown-item" href="#" target="_SELF" title="Access my profile ({{'@'.Auth::user()->username}}).">{{'@'.Auth::user()->username}}</a></li>
-                <li><a class="dropdown-item" href="#" target="_SELF" title="Explore">Explore</a></li>
-                <li><a class="dropdown-item" href="#" target="_SELF" title="Settings">Settings</a></li>
+            <ul class="dropdown-menu text-small shadow text-center">
+                <li><a class="dropdown-item cursor-pointer" href="#" target="_SELF" title="Access my profile ({{'@'.Auth::user()->username}}).">{{'@'.Auth::user()->username}}</a></li>
+                <li><a class="dropdown-item cursor-pointer" href="#" target="_SELF" title="Explore">Explore</a></li>
+                <li><a class="dropdown-item cursor-pointer" href="#" target="_SELF" title="Settings">Settings</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" target="_SELF" title="Sign out" id="btnSignin">Sign out</a></li>
+                <li><a class="dropdown-item cursor-pointer" target="_SELF" title="Sign out" id="btnSignin">Sign out</a></li>
             </ul>
 
         @else
