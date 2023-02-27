360d16cfbd1a6dfd09416c9546130427dba03e66
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-27 13:45:06 -0300

    Design of the user account activation.
    The design of the user account activation page was created.

--- resources/views/ActivateUserAccountController/edit.blade.php
+++ resources/views/ActivateUserAccountController/edit.blade.php
@@ -8,7 +8,61 @@ Activate your account
 
     @include('Components.header')
 
-    <main></main>
+    <main class="sign-container">
+
+        <section class="mb-4 text-center">
+
+            <hgroup>
+                <h1 class="display-3 fw-bold mb-2">Activate your account</h1>
+                <h2 class="display-6 lh-14">Enter the activation token that was sent to your email.</h2>
+            </hgroup>
+        
+        </section>
+
+        <section>
+
+            <div>
+
+                <form action="{{route('accounts.activate.do')}}" method="POST">
+
+                    @csrf
+                    @method('PUT')
+
+                    <div class="d-flex flex-wrap justify-content-center">
+                            
+                        @foreach(str_split(Auth::user()->activation_token, 1) as $digitPosition => $digit)
+                            <input type="text" id="digit_{{$digitPosition}}" name="digit_{{$digitPosition}}" class="input-activation-token" maxlength="1" @if($digitPosition == 0) autofocus @endif>
+                        @endforeach
+                        
+                    </div>
+
+                    <div class="mt-3 text-center">
+                        <input type="submit" value="Ready" title="Ready" class="btn btn-outline-primary">
+                    </div>
+
+                    <div class="mt-3 text-center">
+                        @if($errors->has('system'))
+                            @foreach($errors->get('system') as $error)
+                            <div>
+                                <p class="form-text text-danger">{{$error}}</p>
+                            </div>
+                            @endforeach
+                        @endif
+                    </div>
+
+                </form>
+
+            </div>
+
+            <div class="mt-3 text-center mb-5">
+
+                <a href="{{route('accounts.activate.resend')}}" title="Resend token">Resend token</a>
+
+            </div>
+        
+        </section>
+
+    </main>
 
 @endsection
 
