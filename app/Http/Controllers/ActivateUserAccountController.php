cf19ce07e7ce50ae87ddfb0de8a48837cea358cd
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-02-28 11:55:23 -0300

    Create of account activation logic.

--- app/Http/Controllers/ActivateUserAccountController.php
+++ app/Http/Controllers/ActivateUserAccountController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request as Request;
 use Illuminate\Http\Response as Response;
 use Illuminate\Http\RedirectResponse as RedirectResponse;
 use App\Http\Requests\ActivationRequest as ActivationRequest;
+use Illuminate\Support\Facades\Auth as Auth;
+use \DateTime as DateTime;
+use App\Models\UserActivity as UserActivity;
 
 class ActivateUserAccountController extends Controller {
 
@@ -18,7 +21,37 @@ class ActivateUserAccountController extends Controller {
 
     public function update(ActivationRequest $request): RedirectResponse {
 
-        dd('activate account of authenticated user', $request);
+        $digits = [];
+        
+        foreach ($request->input() as $inputName => $inputValue) {
+
+            if (preg_match("/^(digit_){1}[0-9]+$/", $inputName)) {
+
+                $digits[$inputName] = $inputValue;
+
+            }
+
+        }
+
+        $digits = implode("", $digits);
+
+        if ($digits != Auth::user()->activation_token) {
+
+            UserActivity::quickActivity("Invalid activation attempt.", "Activation attempt with invalid token. Token: {$digits}.", Auth::user()->id);
+
+            return redirect()->back()->withInput()->withErrors([
+                'system' => "Invalid activation token.",
+            ]);
+
+        }
+
+        Auth::user()->activation_at = new DateTime('now');
+        Auth::user()->save();
+
+        $activationToken = Auth::user()->activation_token;
+        UserActivity::quickActivity('Account activated.', "Account was activated with token {$activationToken}.", Auth::user()->id);
+
+        return redirect()->route('explore');
 
     }
 
