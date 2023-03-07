42c3e83ece65f283c0f63f45f212c23d12c5f915
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-03-07 12:24:36 -0300

    The collation of the "activation_token" column has been modified.
    For security reasons, the collation of the "activation_token" column of the "user_accounts" table has been modified to differentiate between uppercase and lowercase characters.

---
+++ database/migrations/2023_03_07_121639_change_collation_activation_token_column_user_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema as Schema;
+use Illuminate\Database\Migrations\Migration as Migration;
+use Illuminate\Database\Schema\Blueprint as Blueprint;
+
+return new class extends Migration {
+
+    public function up() {
+
+        if (Schema::hasTable('user_accounts') && Schema::hasColumn('user_accounts', 'activation_token')) {
+
+            Schema::table('user_accounts', function(Blueprint $table) {
+
+                $table->string('activation_token')->nullable()->collation('utf8mb4_0900_as_cs')->change();
+
+            });
+
+        }
+
+    }
+
+    public function down() {
+
+        if (Schema::hasTable('user_accounts') && Schema::hasColumn('user_accounts', 'activation_token')) {
+
+            Schema::table('user_accounts', function(Blueprint $table) {
+
+                $table->string('activation_token')->nullable()->collation(null)->change();
+
+            });
+
+        }
+
+    }
+
+};
