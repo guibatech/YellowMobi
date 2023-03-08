a17777dca8e785d69ca5ad8fe77a0c2bd9658428
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-03-08 00:06:35 -0300

    Increase size of user activity log fields.
    The fields referring to the user activity log had their sizes increased.

---
+++ database/migrations/2023_03_07_234739_increase_fields_size_user_activities_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema as Schema;
+use Illuminate\Database\Migrations\Migration as Migration;
+use Illuminate\Database\Schema\Blueprint as Blueprint;
+
+return new class extends Migration {
+
+    public function up() {
+
+        if (Schema::hasTable('user_activities')) {
+
+            if (Schema::hasColumn('user_activities', 'activity')) {
+
+                Schema::table('user_activities', function(Blueprint $table) {
+
+                    $table->text('activity')->change();
+
+                });
+
+            }
+
+            if (Schema::hasColumn('user_activities', 'details')) {
+
+                Schema::table('user_activities', function(Blueprint $table) {
+
+                    $table->text('details')->nullable()->change();
+
+                });
+
+            }
+
+        }
+
+    }
+
+    public function down() {
+
+        if (Schema::hasTable('user_activities')) {
+
+            if (Schema::hasColumn('user_activities', 'activity')) {
+
+                Schema::table('user_activities', function(Blueprint $table) {
+
+                    $table->string('activity')->change();
+
+                });
+
+            }
+
+            if (Schema::hasColumn('user_activities', 'details')) {
+
+                Schema::table('user_activities', function(Blueprint $table) {
+
+                    $table->string('details')->nullable()->change();
+
+                });
+
+            }
+
+        }
+
+    }
+
+};
