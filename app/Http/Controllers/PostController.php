6d1ee8597532f2d52a741dad9b2f5fdca6f05d5e
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-03-12 21:58:18 -0300

    Logic for inserting post data.
    Creating logic for inserting post text and post image in the database.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\RedirectResponse as RedirectResponse;
 use App\Models\Post as Post;
 use Illuminate\Support\Facades\Auth as Auth;
 use Illuminate\Support\Facades\Session as Session;
+use App\Models\PostImage as PostImage;
 
 class PostController extends Controller {
 
@@ -17,6 +18,15 @@ class PostController extends Controller {
         $newPost->content = $request->postText;
         $newPost->user_id = Auth::user()->id;
         $newPost->save();
+
+        if ($request->hasFile('postImage')) {
+
+            $newPostImage = new PostImage();
+            $newPostImage->post_id = $newPost->id;
+            $newPostImage->path = $request->postImage->store('post_images');
+            $newPostImage->save();
+
+        }
 
         Session::flash('post-created', "We're sure the world will love to read what you have to say!");
         
