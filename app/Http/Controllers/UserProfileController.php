bbdc7d492483c6397ac76b656895f8a760339c5b
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-03-13 14:32:36 -0300

    Add "Request" parameter in "show" action.
    In "UserProfileController".

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller as Controller;
 use Illuminate\Http\Response as Response;
+use Illuminate\Http\Request as Request;
 
 class UserProfileController extends Controller {
 
-    public function show(string $username): Response {
+    public function show(string $username, Request $request): Response {
 
-        dd($username);
+        dd($username, $request);
 
     }
 
