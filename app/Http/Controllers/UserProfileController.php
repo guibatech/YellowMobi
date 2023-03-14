ac6ed22eb0fdbb3ff47b3b6daefff7e86eb7a46b
Guilherme Barros <guilhermebarros1419@gmail.com>
2023-03-14 17:00:47 -0300

    HTML tags for the profile page.

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller as Controller;
 use Illuminate\Http\Response as Response;
 use Illuminate\Http\Request as Request;
+use App\Models\UserAccount as UserAccount;
 
 class UserProfileController extends Controller {
 
     public function show(string $username, Request $request): Response {
 
-        dd($username, $request);
+        $userFound = UserAccount::where('username', '=', $username)->first();
+
+        return response()->view('UserProfileController.show', [
+            'user' => $userFound,
+        ], 200);
 
     }
 
---
+++ resources/views/UserProfileController/show.blade.php
@@ -0,0 +1,82 @@
+@extends('template')
+
+@section('title')
+
+    @if ($user == null)
+
+        Profile not found!
+
+    @else
+
+        {{$user->profile->name}} ({{'@'.$user->username}})
+    
+    @endif
+
+@endsection
+
+@section('body')
+
+    @include('Components.header')
+
+    <main class="custom-container fast-slide-animation">
+
+        @if ($user == null)
+
+            @include('Components.alert', [
+                'message' => 'This profile does not exist.'
+                ])
+
+        @else
+
+            <section class="mb-4">
+
+                <div>
+                    @include('Components.photo', [
+                        'type' => 'profile',
+                        'user' => $user,
+                        ])
+                </div>
+                
+                <div>
+                    <p>{{$user->profile->name}}</p>
+                    <p>{{'@'.$user->username}}</p>
+                    <p>{{$user->profile->bio}}</p>
+                </div>
+
+            </section>
+
+            <section>
+
+                <div>
+                    <p>Following: 0</p>
+                    <p>Followers: 0</p>
+                </div>
+
+            </section>
+
+            <section>
+
+                <div>
+                    <button>Follow</button>
+                    <button>Unfollow</button>
+                </div>
+
+            </section>
+
+            <section>
+
+                <p>This user's posts will be listed here.</p>
+
+            </section>
+
+        @endif
+
+    </main>
+
+@endsection
+
+@section('scripts')
+
+    @vite('resources/js/header.js')
+
+@endsection
